cb40b381a10becf833cd19862362c1864f3c5dd2
Ruchika Das <dasruchika016@gmail.com>
2023-12-22 15:31:12 +0530

    Create Breadth-first-search.py

---
+++ Breadth-first-search.py
@@ -0,0 +1,14 @@
+from collections import deque
+
+def bfs(graph, start):
+    visited = set()
+    queue = deque([start])
+    visited.add(start)
+
+    while queue:
+        vertex = queue.popleft()
+        print(vertex, end=' ')
+
+        for neighbor in graph[vertex] - visited:
+            queue.append(neighbor)
+            visited.add(neighbor)
