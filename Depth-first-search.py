2a34842c1d91a472e4e10bf7b653a80ea7749221
Ruchika Das <dasruchika016@gmail.com>
2023-12-22 15:30:34 +0530

    Create Depth-first-search.py

---
+++ Depth-first-search.py
@@ -0,0 +1,8 @@
+def dfs(graph, start, visited=None):
+    if visited is None:
+        visited = set()
+    visited.add(start)
+    print(start, end=' ')
+
+    for neighbor in graph[start] - visited:
+        dfs(graph, neighbor, visited)
